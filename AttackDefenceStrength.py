378b3fa660b71c88f905c3231cc93d2c814a86ce
Benjamin Jordan <ben.t.jordan@outlook.com>
2026-06-11 16:13:51 +0100

    Team stats

--- AttackDefenceStrength.py
+++ AttackDefenceStrength.py
@@ -3,14 +3,6 @@ import pandas as pd
 def rating_to_strength(rating, base=1.0):
     return base + (rating - 1500) / 1000
 
-team_stats = {
-    team: {
-        'attack':  rating_to_strength(rating),
-        'defence': 2.0 - rating_to_strength(rating)
-    }
-    for team, rating in fifa_rankings.items()
-}
-
 df = pd.read_csv('/Users/benjaminjordan/Documents/World Cup Model/worldcup/team_rankings.csv')
 
 # Build the rankings dict and team_stats in two lines
@@ -27,4 +19,4 @@ team_stats = {
 df['attack']  = df['Points'].apply(rating_to_strength)
 df['defence'] = df['Points'].apply(lambda r: 2.0 - rating_to_strength(r))
 
-print(df.to_string(index=False))
+df.to_csv('team_stats.csv', index=False)
